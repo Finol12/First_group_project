bd580fa0fbb6339d975210e37c9303d247942393
mfirdaus354 <muhammad.anhar.firdausyi@gmail.com>
2023-06-26 15:43:51 +0200

    Add func find_price to utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests as r
+import re
 
 def get_locality(wp_soup):
     """This function receives a 'soup' from a specific webpage
@@ -18,3 +19,27 @@ def get_url(url):
     soup = BeautifulSoup(page.text, "html.parser")
     return soup
 
+def find_price(immo_soup):
+    in_span = []
+    span_lookup = immo_soup.find_all("span")
+    
+    for things in span_lookup:
+        line_lookup = things.get_text()
+        in_span.append(line_lookup)
+    
+    prices = []
+    for string in in_span:
+        cleaned_string = string.replace("\n", "")
+        match = re.search(r"(?:\b€)?(\d{1,3}(?:[.,]\d{3})*€?)\b", cleaned_string)
+        if match:
+            prices.append(match.group(0).replace(',', ''))
+        else:
+            pass
+    
+    price = 0
+    
+    for num in prices: 
+        if len(num) >= 7:
+            price += int(num) 
+
+    return price
